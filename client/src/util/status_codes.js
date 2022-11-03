3ad5e3d57170f5c8939e90cfde3d5308be9a73f3
vasilisdelas <vasilisdelas@gmail.com>
2022-11-03 12:26:25 +0200

    frontend util

---
+++ client/src/util/status_codes.js
@@ -0,0 +1,6 @@
+const status_codes = {
+    "400": "Couldn't verify credentials.", // Bad Request
+    "200": "OK", // OK
+    "500": "Internal Server Error.", // Internal Server Error
+};
+export default status_codes;
---
+++ client/src/util/user_roles.js
@@ -0,0 +1,6 @@
+const user_roles = {
+    "Admin": 255,
+    "User": 0
+};
+
+export default user_roles;
